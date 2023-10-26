1fbd1068b34555b018a961df325eb85ff2c98c4f
Jun <junhan.dev@gmail.com>
2023-10-26 22:10:00 +0200

    commit 26.10.2023

--- components/utils/app.js
+++ components/utils/app.js
@@ -1,19 +1,21 @@
 'use strict';
 
 import fetchApp from './fetchApp.js';
+import search from './search.js';
+
 import card from '../views/card.js';
-import search from '../utils/search.js';
 import select from '../views/select.js';
 import tag from '../views/select.js';
 
 const app = async () => {
-	const data = await fetchApp();
-	card(data);
-	search(data);
-	select(data);
-	tag(data);
+  const data = await fetchApp();
+  search(data);
 
-	localStorage.setItem('DATA_KEY', JSON.stringify(data));
+  card(data);
+  select(data);
+  tag(data);
+
+  localStorage.setItem('DATA_KEY', JSON.stringify(data));
 };
 
 window.addEventListener('load', app);
