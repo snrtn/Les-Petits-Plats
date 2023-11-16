412c1b2453497431a46e94d0bef0b32545399946
Jun <junhan.dev@gmail.com>
2023-11-16 15:12:39 +0100

    commit 16.11.2023

--- components/utils/fx.js
+++ components/utils/fx.js
@@ -4,20 +4,34 @@ const curry =
   (a, ..._) =>
     _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
-// Définition de la fonction map : utilise la fonction curry pour prendre un objet iterable (iter) et une fonction (f), puis applique la fonction à chaque élément, renvoyant le tableau résultant
+// Définition de la fonction map : utilise la fonction curry pour prendre un objet iterable (iter) et une fonction (f),
+// puis applique la fonction à chaque élément, renvoyant le tableau résultant
 export const map = curry((iter, f) => {
+  // Initialise un tableau vide pour stocker les résultats de la transformation.
   let res = [];
+
+  // Parcourt chaque élément de l'objet iterable (iter).
   for (const a of iter) {
+    // Applique la fonction (f) à l'élément en cours et ajoute le résultat au tableau.
     res.push(f(a));
   }
+
+  // Renvoie le tableau résultant de la transformation.
   return res;
 });
 
-// Définition de la fonction filter : utilise la fonction curry pour prendre un objet iterable (iter) et une fonction de condition (f), puis renvoie un tableau des éléments satisfaisant la condition
+// Définition de la fonction filter : utilise la fonction curry pour prendre un objet iterable (iter) et une fonction de condition (f),
+// puis renvoie un tableau des éléments satisfaisant la condition
 export const filter = curry((iter, f) => {
+  // Initialise un tableau vide pour stocker les éléments satisfaisant la condition.
   let res = [];
+
+  // Parcourt chaque élément de l'objet iterable (iter).
   for (const a of iter) {
+    // Vérifie si l'élément satisfait la condition définie par la fonction (f).
     if (f(a)) res.push(a);
   }
+
+  // Renvoie le tableau résultant des éléments satisfaisant la condition.
   return res;
 });
